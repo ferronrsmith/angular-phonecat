fd393c506ed599ca5494d4c49c623d4af3b00d5d
Igor Minar <iiminar@gmail.com>
2011-03-05 21:03:53 -0800

    step-2
    - Converted static page into dynamic one by:
      - create a root controller for the application
      - extracting the data from HTML into a the controller as a mock dataset
      - convert the static document into a template with the use of `ng:` [directive] (iterate over
        mock data using [ng:repeat] and render it into a view)
    - Added unit test, which mostly shows how one goes about writing a unit test, rather then test
      something of value on our mock dataset.

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,1 +1,10 @@
 /* jasmine specs for controllers go here */
+
+describe('PhonesCtrl', function(){
+
+  it('create phones model with 5 phones', function() {
+    var ctrl = new PhonesCtrl();
+    expect(ctrl.phones).toBeDefined();
+    expect(ctrl.phones.length).toBe(5);
+  });
+});
