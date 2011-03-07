2a934c2109188cc48c831949c6efa64633ebda71
Misko Hevery <misko@hevery.com>
2011-03-06 16:26:30 -0800

    step-7
    - Introduce the [$route] service which allows binding URLs for deep-linking with views
      - Replace content of root controller PhonesCtrl with [$route] configuration
      - Map `/phones' to PhoneListCtrl and partails/phones-list.html
      - Map `/phones/phone-id' to PhoneDetailCtrl and partails/phones-detail.html
      - Copy deep linking parameters to root controller `params` property for access in sub controllers
      - Replace content of index.html with [ng:view]
    - Create PhoneListCtrl view
      - Move code which fetches phones data into PhoneListCtrl
      - Move existing HTML from index.html to partials/phone-list.html
    - Create PhoneDetailsCtrl view
      - Wire [$route] service to map `/phanes/phone-id` to map to this controller.
      - Empty PhoneDetailsCtrl
      - Place holder partials/phane-details.html

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,6 +1,23 @@
 /* App Controllers */
 
-function PhonesCtrl($xhr) {
+function PhonesCtrl($route) {
+  var self = this;
+
+  $route.parent(this);
+
+  $route.when('/phones',
+             {template:'partials/phone-list.html',   controller:PhoneListCtrl});
+  $route.when('/phones/:phoneId',
+             {template:'partials/phone-detail.html', controller:PhoneDetailCtrl});
+
+  $route.otherwise({redirectTo:'/phones'});
+
+  $route.onChange(function(){
+    self.params = $route.current.params;
+  });
+}
+
+function PhoneListCtrl($xhr) {
   var self = this;
 
   self.orderProp = 'age';
@@ -8,4 +25,8 @@ function PhonesCtrl($xhr) {
   $xhr('GET', 'phones/phones.json', function(code, response) {
     self.phones = response;
   });
+}
+
+function PhoneDetailCtrl() {
+
 }
